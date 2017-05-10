6c201c24ce2d953f0abcf8d0ac6482ec9887c6eb
ej <ejaramos@gmail.com>
2017-05-10 08:36:38 -0400

    added more rule scenarios

--- src/main/java/com/sample/DataPacket.java
+++ src/main/java/com/sample/DataPacket.java
@@ -4,11 +4,19 @@ public class DataPacket {
 	private static char category;
 	private static double gauss;
 	private static int random;
+	private static int random2;
+	private static int random3;
+	private static int random4;
 	
-	public DataPacket(char category, double gauss, int random){
+	public DataPacket(
+			char category, double gauss, int random, 
+			int random2, int random3, int random4){
 		this.setCategory(category);
 		this.setGauss(gauss);
 		this.setRandom(random);
+		this.setRandom2(random2);
+		this.setRandom3(random3);
+		this.setRandom4(random4);
 	}
 	
 	public char getCategory(){
@@ -35,8 +43,42 @@ public class DataPacket {
 		DataPacket.random = random;
 	}
 	
+	public int getRandom2() {
+		return random2;
+	}
+
+	public void setRandom2(int random2) {
+		DataPacket.random2 = random2;
+	}
+
+	public int getRandom3() {
+		return random3;
+	}
+
+	public void setRandom3(int random3) {
+		DataPacket.random3 = random3;
+	}
+
+	public int getRandom4() {
+		return random4;
+	}
+
+	public void setRandom4(int random4) {
+		DataPacket.random4 = random4;
+	}
+	
+
 	public String toString(){
-		return "TEST";
+		return String.join(",", 
+			String.valueOf(DataPacket.category), 
+			String.valueOf(DataPacket.gauss),
+			String.valueOf(DataPacket.random),
+			String.valueOf(DataPacket.random2),
+			String.valueOf(DataPacket.random3),
+			String.valueOf(DataPacket.random4)
+				
+			) ;
+			
 	}
 	
 }
--- src/main/java/com/sample/RulesRunner.java
+++ src/main/java/com/sample/RulesRunner.java
@@ -29,21 +29,41 @@ public class RulesRunner {
             KieServices ks = KieServices.Factory.get();
             KieContainer kContainer = ks.getKieClasspathContainer();
             KieBaseConfiguration kconfig = KieServices.Factory.get().newKieBaseConfiguration();
-            kconfig.setOption(RuleEngineOption.PHREAK);
-            //kconfig.setOption(RuleEngineOption.RETEOO);
+            
+            boolean phreakFlag = false;
+            String outputString;
+            if (phreakFlag) {
+            	kconfig.setOption(RuleEngineOption.PHREAK);
+            	outputString = ", PHREAK";
+            }
+            else {
+            	kconfig.setOption(RuleEngineOption.RETEOO);
+            	outputString = ", RETEOO";
+            }
+            
             KieSession kSession = kContainer.newKieSession("ksession-rules");
             int Rule_hits = 0;
             kSession.setGlobal("Rule1_hits", Rule_hits);
-            
+            int lineCount = 0;
             br = new BufferedReader(new FileReader(csvFile));
 			while((line = br.readLine()) != null){
-				
+				lineCount += 1;
 				Object[] raw = line.split(delimiter);
 				char categoricalProperty = raw[0].toString().toCharArray()[0];
 				double gaussianProperty = Double.parseDouble(raw[1].toString());
 				int randomProperty = Integer.parseInt(raw[2].toString());
+				int randomProperty2 = Integer.parseInt(raw[3].toString());
+				int randomProperty3 = Integer.parseInt(raw[4].toString());
+				int randomProperty4 = Integer.parseInt(raw[5].toString());
 			
-				DataPacket  packet = new DataPacket(categoricalProperty, gaussianProperty, randomProperty);
+				DataPacket  packet = new DataPacket(
+						categoricalProperty, 
+						gaussianProperty, 
+						randomProperty, 
+						randomProperty2,
+						randomProperty3,
+						randomProperty4
+						);
 	            kSession.insert(packet);
 	            kSession.fireAllRules();
 			}
@@ -60,11 +80,16 @@ public class RulesRunner {
 			String durationToString = String.format("%02d min, %02d sec, %03d ms",
 					minutes, seconds, millis
 					);
-            System.out.println("Duration = " + durationToString);
+            System.out.print(
+        			"--> "+ lineCount
+    				+ " lines, " 
+    				+ durationToString
+    				+ outputString
+            		);
              
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+	        } catch (Exception ex) {
+	            ex.printStackTrace();
+	        }
     }
 //    
 //    public class Helper(){
